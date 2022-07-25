cb69094a96e5fb974961c6bc70767c01db3e6c9d
Evgenij Shka <stirelshka8@gmail.com>
2022-07-25 21:36:14 +0300

    Поехали!

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 import requests
 
+# TODO: Код ниже! Выдает стату. Осталось написать код поиска по ID и дело в шляпе
 # number_id = 379
 # url = f"https://akabab.github.io/superhero-api/api/powerstats/{number_id}.json"
+
+
+
+
 list_superhero = ['Hulk']
 
 resp = requests.get(url).json()

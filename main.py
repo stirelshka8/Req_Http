860690006a5fdbbbb64ec63049630193dc96eac5
Evgenij Shka <stirelshka8@gmail.com>
2022-07-25 21:47:41 +0300

    Что то с чем то!

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
 import requests
 
+class SuperHero_ID:
+    def __init__ (self):
+        self.list_superhero = ["Hulk", "Captain America", "Thanos"]
+
+    def search(self, url = "https://akabab.github.io/superhero-api/api/all.json"):
+        self.url = url
+        self.data_hero  = requests.get(self.url).json()
+        return self.data_hero
+            
+    
+
+
+
+
 # TODO: Код ниже! Выдает стату. Осталось написать код поиска по ID и дело в шляпе
 # number_id = 379
 # url = f"https://akabab.github.io/superhero-api/api/powerstats/{number_id}.json"
 
+supher = SuperHero_ID()
 
-
-
-list_superhero = ['Hulk']
-
-resp = requests.get(url).json()
-print(resp)
+print(supher.search())

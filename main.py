56ab84188656c6ce34da94b149c2eb2478c5ca6a
Evgenij Shka <83755865+stirelshka8@users.noreply.github.com>
2022-07-28 22:05:55 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,6 @@ print(f"[INFO] Самый умный супергерой --> {hero.smart_hero()
 
 if __name__ == '__main__':
     path_to_file = '//home//stirelshka//Python.jpg'
-    token = 'AQAAAAAaGso_AADLW3qdanTLIEX2nZks0A3bLNE'
+    token = '00000000000000000000000000000'
     uploader = YaUploader(token)
     result = uploader.upload(path_to_file)

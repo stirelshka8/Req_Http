1a87abf4b841d97b3aac76eb52b1688a30540709
Evgenij Shka <stirelshka8@gmail.com>
2022-07-28 22:04:30 +0300

    2 задание готово

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import hero
 from yaupload import YaUploader
 
-#print(f"[INFO] Самый умный супергерой --> {hero.smart_hero()}")
+print(f"[INFO] Самый умный супергерой --> {hero.smart_hero()}")
 
 if __name__ == '__main__':
-    path_to_file = '/media/stirelshka/Рабочий/PyProj/NETOLOGY/Req_Http/UpFile/Python.jpg'
+    path_to_file = '//home//stirelshka//Python.jpg'
     token = 'AQAAAAAaGso_AADLW3qdanTLIEX2nZks0A3bLNE'
     uploader = YaUploader(token)
     result = uploader.upload(path_to_file)
--- yaupload.py
+++ yaupload.py
@@ -5,8 +5,20 @@ class YaUploader:
     def __init__(self, token: str):
         self.token = token
 
-    def upload(self):
-        """Метод загружает файлы по списку file_list на яндекс диск"""
+    def upload(self, file_path: str):
+        self.file_path = file_path.split('/')
+        self.file_name = self.file_path[-1]
+        self.upload_url = 'https://cloud-api.yandex.net/v1/disk/resources/upload'
+        self.head = ({'Content-Type': 'application/json', 'Authorization': 'OAuth {}'.format(self.token)})
+        self.setting = {'path': self.file_name, 'overwrite': 'true'}
+        self.send = requests.get(self.upload_url, headers=self.head, params=self.setting)
+        #print(self.send.json())
+        self.work_url = self.send.json().get("href","")
+        #print(self.work_url)
+        self.create_path = self.file_name.strip('.jpg')
+        #print(self.create_path)
+        self.upload_file = requests.put(self.work_url, data=self.file_name)
+        self.upload_file.raise_for_status()
 
 
 
